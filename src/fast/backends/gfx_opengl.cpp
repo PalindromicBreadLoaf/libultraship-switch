5d38a6091269ca6a0beefcd5036057e9218282da
PalindromicBreadLoaf <palindromicbreadloaf@tuta.com>
2026-07-31 21:01:07 -0400

    Port Nintendo Switch support over

--- src/fast/backends/gfx_opengl.cpp
+++ src/fast/backends/gfx_opengl.cpp
@@ -779,7 +779,7 @@ void GfxRenderingAPIOGL::UploadTexture(const uint8_t* rgba32_buf, uint32_t width
     textures[mCurrentTextureIds[mCurrentTile]].height = height;
 }
 
-#ifdef USE_OPENGLES
+#if defined(__SWITCH__) || defined(USE_OPENGLES)
 #define GL_MIRROR_CLAMP_TO_EDGE 0x8743
 #endif
 
@@ -921,7 +921,7 @@ void GfxRenderingAPIOGL::DrawTriangles(float buf_vbo[], size_t buf_vbo_len, size
 }
 
 void GfxRenderingAPIOGL::Init() {
-#if !defined(__linux__) && !defined(__OpenBSD__)
+#if !defined(__SWITCH__) && !defined(__linux__) && !defined(__OpenBSD__)
     glewInit();
 #endif
 
